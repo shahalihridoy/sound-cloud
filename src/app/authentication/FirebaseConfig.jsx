505386c21821192d18afb3061532f07a857ad974
Md Shah Ali Hriody <31925919+shahalihridoy@users.noreply.github.com>
2019-07-24 00:15:34 +0600

    api key deleted

--- src/app/authentication/FirebaseConfig.jsx
+++ src/app/authentication/FirebaseConfig.jsx
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDNqU_-YLiudfO93w0uiDqOuCGxlA9J0uY",
-  authDomain: "sound-cloud-1a470.firebaseapp.com",
-  databaseURL: "https://sound-cloud-1a470.firebaseio.com",
-  projectId: "sound-cloud-1a470",
-  storageBucket: "sound-cloud-1a470.appspot.com",
-  messagingSenderId: "241098932958",
-  appId: "1:241098932958:web:456cece967e97225"
 };
 
 firebase.initializeApp(firebaseConfig);
